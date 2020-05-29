932c5ac6d5c95e4657cbfc3aa7f231719976cfb6
floss-order <flossOrder@gmail.com>
2020-05-29 19:27:37 +0300

    Server initial startup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const path = require('path')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+
+//Server config
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.set('layout', 'layouts/layout.ejs')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded( { limit: '10mb', extended: false }))
+app.use(methodOverride('_method'))
+
+//Routes list
+const indexRouter = require('./routes/index.js')
+app.use('/', indexRouter)
+
+//Server startup
+app.listen(process.env.PORT || 3000)
