6c4157310aed4b0b7ed0498dc8d1d8881f4331ff
floss-order <flossOrder@gmail.com>
2020-05-30 09:50:36 +0300

    Table router finally can handle requests

--- routes/tables.js
+++ routes/tables.js
@@ -3,6 +3,32 @@ const router = express.Router()
 const db = require('../db.js')
 
 
+//Recievers route
+router.get('/recievers', async (req, res) => {
+    try {
+        let results = await db.getRecievers()
+        res.render('tables/recievers.ejs', {results})
+    }
+    catch(e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
+router.post('/recievers', async (req, res) => {
+    let queryString = req.body.query
+    console.log(queryString)
+    try {
+        await db.query(queryString)
+        res.redirect('/tables/recievers')
+    }
+    catch(e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
+//Subscriptions route
 router.get('/subscriptions', async (req, res) => {
     try {
         let results = await db.getSubscriptions()
@@ -16,9 +42,35 @@ router.get('/subscriptions', async (req, res) => {
 
 router.post('/subscriptions', async (req, res) => {
     let queryString = req.body.query
+    console.log(queryString)
     try {
         await db.query(queryString)
-        res.redirect('subscriptions/')
+        res.redirect('/tables/subscriptions')
+    }
+    catch(e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
+//Editions route
+router.get('/editions', async (req, res) => {
+    try {
+        let results = await db.getSubscriptions()
+        res.render('tables/editions.ejs', {results})
+    }
+    catch(e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
+router.post('/editions', async (req, res) => {
+    let queryString = req.body.query
+    console.log(queryString)
+    try {
+        await db.query(queryString)
+        res.redirect('/tables/editions')
     }
     catch(e) {
         console.log(e)
@@ -27,4 +79,31 @@ router.post('/subscriptions', async (req, res) => {
 })
 
 
+//Editions that has subscriptions route
+router.get('/editions_has_subscriptions', async (req, res) => {
+    try {
+        let results = await db.getSubscriptions()
+        res.render('tables/editions_has_subscriptions.ejs', {results})
+    }
+    catch(e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
+router.post('/editions_has_subscriptions', async (req, res) => {
+    let queryString = req.body.query
+    console.log(queryString)
+    try {
+        await db.query(queryString)
+        res.redirect('/tables/editions_has_subscriptions')
+    }
+    catch(e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
+
+//Export tables router
 module.exports = router
